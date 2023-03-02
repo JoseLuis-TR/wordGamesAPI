927cfb776a21ccf194bf33745fb005713d36df7b
JoseLuis-TR <jtocroj094@g.educaand.es>
2023-03-02 19:35:16 +0100

    Initial commit

---
+++ src/main/java/com/api/wordgames/WordGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.wordgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordGamesApiApplication.class, args);
+	}
+
+}
